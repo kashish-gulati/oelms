6e823c9f7533aafaf95167bccfc5da0e27a72e3e
kashish-gulati <kashlati@gmail.com>
2024-06-24 01:35:55 +0530

    added library manager

---
+++ libarymanager/src/test/java/com/oelms/librarymanager/LibrarymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oelms.librarymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
